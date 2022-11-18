65379399b95b944167bdd838fff294a684903c54
NekoYo1 <kostyafear203@gmail.com>
2022-11-18 16:55:07 +1000

    Вроде сделал пикча в ресурсах есть но размер наверн большой

--- WPFFILIPPOV_PR7/MainWindow.xaml.cs
+++ WPFFILIPPOV_PR7/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace WPFFILIPPOV_PR7
         {
             InitializeComponent();
         }
+        int summ;
+        private void M_g_Checked(object sender, RoutedEventArgs e)
+        {
+            Itog.Text = Itog.Text + "1. Геркулес 200 рублей";
+            int cvn1;
+            cvn1 = Convert.ToInt32(Itog_3.Text);
+            summ = cvn1 + 200;
+            Itog_3.Text = summ.ToString();
+        }
+
+        private void By(object sender, RoutedEventArgs e)
+        {
+            Itog.Text = Itog.Text + "2. Булочка с сгущенкой 20 рублей ";
+            int cvn2;
+            cvn2 = Convert.ToInt32(Itog_3.Text);
+            summ = cvn2 + 20;
+            Itog_3.Text = summ.ToString();
+        }
+
+        private void hai(object sender, RoutedEventArgs e)
+        {
+            Itog.Text = Itog.Text + "3. Чай 10 рублей ";
+            int cvn3;
+            cvn3 = Convert.ToInt32(Itog_3.Text);
+            summ = cvn3 + 10;
+            Itog_3.Text = summ.ToString();
+
+        }
+        private void MK(object sender, RoutedEventArgs e)
+        {
+            Itog.Text = Itog.Text + "4. Котлета свиная 50 рублей ";
+            int cvn4;
+            cvn4 = Convert.ToInt32(Itog_3.Text);
+            summ = cvn4 + 50;
+            Itog_3.Text = summ.ToString();
+
+        }
+        private void M_KM(object sender, RoutedEventArgs e)
+        {
+            Itog.Text = Itog.Text + "5. Картошка с мясом 250 рублей ";
+            int cvn5;
+            cvn5 = Convert.ToInt32(Itog_3.Text);
+            summ = cvn5 + 250;
+            Itog_3.Text = summ.ToString();
+
+        }
     }
 }
